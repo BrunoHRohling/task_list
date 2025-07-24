54e1d9d506fb22bb8c05a124b15be5b7d1bb4074
Bruno <brunorohling340@gmail.com>
2025-07-23 22:08:17 -0300

    task_list classes 12 - 13

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,15 @@
-ola
-@isset($name)
-acesse meu nome com: {{$name}}
-@endisset
+<h1>
+  The list of tasks
+</h1>
 
+<div>
+  {{-- @if (count($tasks)) --}}
+  @forelse ($tasks as $task)
+    <div>
+      <a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a>
+    </div>
+  @empty
+    <div>There are no tasks!</div>
+  @endforelse
+  {{-- @endif --}}
+</div>
