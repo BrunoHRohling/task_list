357143c5ad6eaad3b5d458de313eadf8103d498e
Bruno <brunorohling340@gmail.com>
2025-08-10 09:20:38 -0300

    Aula 21 Aprendi a gerar dados falsos para teste (factory e seeders). Aprendi a criar tabelas utilizando prompt. Criar formulários com csrf tokens e validações iniciais dos dados. Salvar dados enviados no banco de dados.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Task;
+use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 
@@ -14,78 +16,42 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-class Task
-{
-    public function __construct(
-        public int $id,
-        public string $title,
-        public string $description,
-        public ?string $long_description,
-        public bool $completed,
-        public string $created_at,
-        public string $updated_at
-    ) {}
-}
-
-$tasks = [
-    new Task(
-        1,
-        'Buy groceries',
-        'Task 1 description',
-        'Task 1 long description',
-        false,
-        '2023-03-01 12:00:00',
-        '2023-03-01 12:00:00'
-    ),
-    new Task(
-        2,
-        'Sell old stuff',
-        'Task 2 description',
-        null,
-        false,
-        '2023-03-02 12:00:00',
-        '2023-03-02 12:00:00'
-    ),
-    new Task(
-        3,
-        'Learn programming',
-        'Task 3 description',
-        'Task 3 long description',
-        true,
-        '2023-03-03 12:00:00',
-        '2023-03-03 12:00:00'
-    ),
-    new Task(
-        4,
-        'Take dogs for a walk',
-        'Task 4 description',
-        null,
-        false,
-        '2023-03-04 12:00:00',
-        '2023-03-04 12:00:00'
-    ),
-];
-
 Route::get('/', function () {
     return redirect()->route('tasks.index');
 });
 
-Route::get('/tasks', function () use ($tasks) {
+Route::get('/tasks', function () {
     return view('index', [
-        'tasks' => $tasks
+        'tasks' => Task::latest()->get()
     ]);
 })->name('tasks.index');
 
-Route::get('/tasks/{id}', function ($id) use ($tasks) {
-    $task = collect($tasks)->firstWhere('id', $id); /// pelo o que entendi, o collect converte o array em um collection object do laravel
-    // o firstWhere pega o primeiro item que bater o valor com a key
+Route::view('/tasks/create', 'create')->name('tasks.create');
 
-    if (!$task) {
-        abort(Response::HTTP_NOT_FOUND);
-    }
-
-    return view('show', ['task' => $task]);
+// pelo o que entendi, o collect converte o array em um collection object do laravel
+// $task = collect($tasks)->firstWhere('id', $id);
+// o firstWhere pega o primeiro item que bater o valor com a key
+Route::get('/tasks/{id}', function ($id) {
+    // find() busca pela chave primaria da classe chamada. Ex: Task::find($id)
+    return view('show', ['task' => Task::findOrFail($id)]);
 })->name('tasks.show');
+
+Route::post('/tasks', function (Request $request) {
+    $data = $request->validate([ // Valida os campos da requisição conforme as regras definidas
+        'title' => 'required|max:255',
+        'description' => 'required',
+        'long_description' => 'required',
+    ]);
+
+    $task = new Task; // Cria instância da classe Task e adiciona aos atributos da classe seus respectivos novos valores
+    $task->title = $data['title'];
+    $task->description = $data['description'];
+    $task->long_description = $data['long_description'];
+
+    $task->save(); // Salva nova task na tabela Task
+
+    return redirect()->route('tasks.show', ['id' => $task->id]);
+})->name('tasks.store');
 
 // Route::get('/xxx', function () {
 //     return 'Hello';
