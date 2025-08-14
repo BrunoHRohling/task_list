5147e170e36d3e1fa2b79141fe359e0fa415bfa7
Bruno <brunorohling340@gmail.com>
2025-08-13 21:33:34 -0300

    pagination and reuse both create and update forms

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,4 +12,11 @@
         <div>There are no tasks!</div>
     @endforelse
 
+    {{-- Pagination --}}
+    @if ($tasks->count())
+        <nav>
+            {{ $tasks->links() }}
+        </nav>
+    @endif
+
 @endsection
